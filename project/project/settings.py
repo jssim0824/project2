273e28c3447ebfdafb4a442466ab52295ecfe43e
SIM <sim9609@naver.com>
2021-02-17 16:19:13 +0900

    weweweeew

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ar$p7kvfhkqqcyyk0f#mi3%w9yz=$w_^ek0d%jmt7oy=ccwj_r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
